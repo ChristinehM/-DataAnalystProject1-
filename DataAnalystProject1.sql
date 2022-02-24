fbb6d951711747100dd42626c2bc1a2533dc58c9
ChristinehM <100333532+ChristinehM@users.noreply.github.com>
2022-02-24 13:30:23 -0800

    Update DataAnalystProject1.sql

--- DataAnalystProject1.sql
+++ DataAnalystProject1.sql
@@ -13,21 +13,21 @@ from [DataAnalystProject1]..['COVID-19 Deaths']
 where [continent] is not null
 order by 1,2
 
---------------------------------------Covid_Deaths Rate------------------------------------------------------------------
+-----------------------------------------Covid_Deaths Rate------------------------------------------------------------------
 
 select [location],[date],[total_cases],[total_deaths],([total_deaths]/[total_cases])*100 as [DeathPercentage]
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
 where [location] like '%states%'
 order by 1,2 
 
---------------------------------------Covid_Cases Rate-----------------------------------------------------------------
+-------------------------------------------Covid_Cases Rate-----------------------------------------------------------------
 
 select [location],[date],[population],[total_cases],([total_cases]/[population])*100 as [PercentPopulationInfected]
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
 where [location] like '%states%'
 order by 1,2 
 
-----------------------------------Highest Infection Rate---------------------------------------------------------------
+-----------------------------------------Highest Infection Rate---------------------------------------------------------------
 
 select [location],[population],Max([total_cases]) as [HighestInfectionCount],Max([total_cases]/[population])*100 as [PercentPopulationInfected]
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
@@ -36,14 +36,14 @@ where [continent] is not null
 Group by [location],[population]
 order by [PercentPopulationInfected] desc
 
--------------------------------Countries with Highest Death Count per Population-------------------------------------
+----------------------------------------Countries with Highest Death Count per Population-------------------------------------
 select [location],Max(cast([total_deaths]as int)) as [TotalDeathCount]
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
 where [continent] is not null
 Group by [location]
 order by [TotalDeathCount] desc
 
---------------------------------Continent with Highest Death Count per Population-------------------------------------
+----------------------------------------Continent with Highest Death Count per Population-------------------------------------
 
 select [continent],Max(cast([total_deaths]as int)) as [TotalDeathCount]
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
@@ -51,7 +51,7 @@ where [continent] is not null
 Group by [continent]
 order by [TotalDeathCount] desc
 
------------------------------------------Global Numbers--------------------------------------------------------------
+--------------------------------------------------Global Numbers--------------------------------------------------------------
 
 select Sum([new_cases]) as [total_cases],Sum(cast([new_deaths] as int)) as total_deaths,Sum(cast([new_deaths] as int))/Sum([new_cases])*100 as DeathPercentage
 from [DataAnalystProject1]..['COVID-19 Deaths'] 
@@ -59,7 +59,7 @@ where [continent] is not null
 --Group by [date]
 order by 1,2
 
-------------------------------------Percentage of Population that has recieved at least one Covid_Vaccine---------------------------------------------
+------------------------------------Percentage of Population that has recieved at least one Covid_Vaccine----------------------
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(Bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
@@ -71,7 +71,7 @@ Join [DataAnalystProject1]..['COVID-19 Vaccinations'] vac
 where dea.continent is not null 
 order by 2,3
 
-----------------------------------Using CTE to perform Calculation on Partition By in previous query-------------------------------------------------------------
+----------------------------------Using CTE to perform Calculation on Partition By in previous query------------------------------
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -88,7 +88,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
 
-----------------------------------------------Temp Table and using Drop Table-------------------------------------------------------------
+------------------------------------------------Temp Table and using Drop Table------------------------------------------------------
 Drop Table if exists #PercentPopulationVaccinated
 create Table #PercentPopulationVaccinated(
 
@@ -112,7 +112,7 @@ Join [DataAnalystProject1]..['COVID-19 Vaccinations'] vac
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
----------------------------------Creating View to store data for later visualizations---------------------------
+--------------------------------------------------Creating View to store data for later visualizations---------------------------
 
 Create view PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
